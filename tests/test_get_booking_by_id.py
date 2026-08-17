8577534864542fb7e0c14004944cf4c1c1e7a568
Ajay Kumar <16305257+blitzajay@users.noreply.github.com>
2026-08-17 11:31:47 +0530

    Add booking by ID test

---
+++ tests/test_get_booking_by_id.py
@@ -0,0 +1,56 @@
+import requests
+
+BASE_URL = "https://restful-booker.herokuapp.com"
+
+def test_get_booking_by_id():
+    # First request: obtain valid booking IDs
+
+    list_response = requests.get(BASE_URL+"/booking", timeout=10)
+    assert list_response.status_code == 200
+
+    bookings = list_response.json()
+    assert len(bookings) > 0
+
+    booking_id = bookings[0]["bookingid"]
+
+    # Second request: retrieve the selected booking
+
+    booking_response = requests.get(f"{BASE_URL}/booking/{booking_id}", timeout=10)
+
+    assert booking_response.status_code == 200
+
+    booking = booking_response.json()
+
+    print("Booking ID : ", booking_id)
+    print("Booking : ", booking)
+
+
+    assert isinstance(booking, dict)
+
+    assert "firstname" in booking
+    assert isinstance(booking["firstname"], str)
+    assert booking["firstname"].strip() != ""
+
+    assert "lastname" in booking
+    assert isinstance(booking["lastname"], str)
+
+    assert "totalprice" in booking
+    assert isinstance(booking["totalprice"], int)
+    assert booking["totalprice"] >= 0
+
+    assert "depositpaid" in booking
+    assert isinstance(booking["depositpaid"], bool)
+    assert "bookingdates" in booking
+
+    assert isinstance(booking["bookingdates"], dict)
+
+    booking_dates = booking["bookingdates"]
+
+    assert "checkin" in booking_dates
+    assert "checkout" in booking_dates
+    assert isinstance(booking_dates["checkin"], str)
+    assert isinstance(booking_dates["checkout"], str)
+
+    assert "application/json" in booking_response.headers["Content-Type"]
+    assert booking_response.elapsed.total_seconds() < 5
+
